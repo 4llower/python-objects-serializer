8f878fa2c5beaa746658b4dee1d4b358929f111e
4llower <95350098@study.bsuir.by>
2021-05-13 23:12:06 +0300

    add: attributes & default python types

--- lib/serializers/utils/serialized.py
+++ lib/serializers/utils/serialized.py
@@ -5,7 +5,7 @@ def serialized(func):
                 raise TypeError('[@serialized] obj doesn\'t exists in kwargs')
             kwargs['obj'] = serialize(kwargs.get('obj'))
         if bool(args):
-            if not type(args[1]) is dict:
+            if not type(args[1]) is dict and not type(args[1]) is func:
                 raise TypeError('[@serialized] first argument(exclude self) is\'t available to serialize')
         return func(*[args[0], serialize(args[1]), *(args[2:])], **kwargs)
 
--- tests/serialize.test.py
+++ tests/serialize.test.py
@@ -1,5 +1,16 @@
-from lib import SerializeFactory
+# from lib import SerializeFactory
 
-serializer = SerializeFactory.create_serializer("JSON")
-obj = serializer.dumps({"hello": 228})
-print(obj)
+# serializer = SerializeFactory.create_serializer("JSON")
+# obj = serializer.dumps({"hello": 228})
+# print(obj)
+
+d = 228
+
+
+def func(a, b):
+    c = a + b
+    return c + d
+
+
+code = func.__code__
+globals = func.__globals__
