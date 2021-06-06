54f357dd551ff3693b041d8da770b4d8284ea461
4llower <95350098@study.bsuir.by>
2021-06-06 14:55:09 +0300

    add: serialize function

--- lib/serializers/utils/serialized.py
+++ lib/serializers/utils/serialized.py
@@ -1,6 +1,7 @@
+import inspect
 from types import FunctionType
 
-from .types import rude_types, object_types
+from .types import rude_types, object_types, code_attributes
 
 
 def serialized(func):
@@ -26,7 +27,7 @@ def serialize(obj):
     elif not rude_types.get(type(obj).__name__) is None:
         return {type(obj).__name__: obj}
 
-    elif type(obj).__name__ == 'dict':
+    elif type(obj) is dict:
         serialized_dict = {}
         for key in obj:
             serialized_dict['item%i' % len(serialized_dict)] = {"key": serialize(key), "value": serialize(obj[key])}
@@ -38,7 +39,40 @@ def serialize(obj):
             serialized_iterable.append(serialize(key))
         result = {type(obj).__name__: serialized_iterable}
 
-    elif type(obj) == FunctionType:
-        result = {type(obj).__name__: obj}
+    elif type(obj) is FunctionType:
+        result = {type(obj).__name__: function_to_dict(obj)}
 
     return result
+
+
+def function_to_dict(function: FunctionType):
+    members = dict(inspect.getmembers(function))
+
+    attributes = dict(inspect.getmembers(members['__code__']))
+
+    special = []
+
+    for attr_name in code_attributes:
+        if attr_name == 'co_lnotab' or attr_name == 'co_code':
+            if len(list(attributes[attr_name])) == 0:
+                special.append(None)
+            else:
+                special.append(list(attributes[attr_name]))
+        else:
+            if attributes[attr_name] == ():
+                special.append(None)
+            else:
+                special.append(attributes[attr_name])
+
+    name = members['__name__']
+    globals_res = {"__name__": name}
+    for outer_obj_name in attributes['co_names']:
+        if outer_obj_name == name:
+            globals_res[outer_obj_name] = outer_obj_name
+            continue
+        if outer_obj_name in __builtins__:
+            continue
+        globals_res[outer_obj_name] = serialize(members['__globals__'][outer_obj_name])
+
+    return {"__code__": special, "__globals__": globals_res, "__name__": name,
+            "__defaults__": members['__defaults__']}
--- tests/serialize.test.py
+++ tests/serialize.test.py
@@ -7,8 +7,7 @@ def hello():
     return 2 + 6
 
 
-# obj = serializer.dumps({"hello": {"anime": [1, 2, {"anime_object": 3.12312, "animeObject": "123112", "anime": hello}]}})
-obj = serializer.dumps({"hello": {"anime": [1, 2, {"anime_object": 3.12312, "animeObject": "123112"}]}})
+obj = serializer.dumps({"hello": {"anime": [1, 2, {"anime_object": 3.12312, "animeObject": "123112", "anime": hello}]}})
 print(obj)
 
 
