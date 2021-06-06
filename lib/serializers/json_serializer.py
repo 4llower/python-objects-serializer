50ce3bca8f89714d570e5ec9691819da00b02573
4llower <95350098@study.bsuir.by>
2021-06-06 18:45:09 +0300

    fix: util

--- lib/serializers/json_serializer.py
+++ lib/serializers/json_serializer.py
@@ -1,10 +1,11 @@
-from .common_serializer import CommonSerializer
 import json
 
+from .common_serializer import CommonSerializer
 from .utils import deserialized, serialized
 
 
 class JSONSerializer(CommonSerializer):
+    @serialized
     def dump(self, obj, fp):
         return json.dump(obj, fp)
 
@@ -12,6 +13,7 @@ class JSONSerializer(CommonSerializer):
     def dumps(self, obj):
         return json.dumps(obj)
 
+    @deserialized
     def load(self, fp):
         return json.load(fp)
 
